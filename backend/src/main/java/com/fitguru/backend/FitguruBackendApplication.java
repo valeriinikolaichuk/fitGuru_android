d687637725c08aba18ca983b6c8d25b84e33a1a6
valeriinikolaichuk <powerlift.rv@gmail.com>
2026-05-03 08:14:12 +0200

    initial commit

---
+++ backend/src/main/java/com/fitguru/backend/FitguruBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fitguru.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitguruBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitguruBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fitguru/backend/FitguruBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitguru.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitguruBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
